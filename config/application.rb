737d4bebda2e712fd3197580e63da295ce013889
Tarek <83262890+tarek102@users.noreply.github.com>
2022-12-09 18:16:39 +0200

    change password column to password_digest

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module DoctorAppointmentBackend
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::CookieStore
+    config.middleware.use ActionDispatch::Session::CookieStore
     config.action_dispatch.cookies_same_site_protection = :strict 
   end
 end
