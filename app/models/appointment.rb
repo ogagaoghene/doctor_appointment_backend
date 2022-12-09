528ec58bdb2195c5a80ecab103f5ed419331210b
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-12-09 13:35:34 +0100

    Add validations to models

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Appointment < ApplicationRecord
+  belongs_to :user
+  belongs_to :doctor
+  validates :user_id, :doctor_id, :city, :appointment_time, presence: true
 end
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class Doctor < ApplicationRecord
+  has_many :appointments, dependent: :destroy
+  
+  has_one_attached :image
+  validates :name, presence: true, length: { in: 3..50, message: 'Name\'s length should be in range of 3 to 50' }
+  validates :speciality, presence: true,
+                         length: { in: 3..100, message: 'Speciality\'s length should be in range of 3 to 100' }
+  validates :fee, presence: true
 end
