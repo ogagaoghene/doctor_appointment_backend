760d03116f5e65772773fcbcee52def7e257e6e4
Tarek <83262890+tarek102@users.noreply.github.com>
2022-12-12 18:54:46 +0200

    fix login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,18 +1,33 @@
 class Api::V1::SessionsController < ApplicationController
-  def create 
-    user = User
-            .find_by(name: params["user"]["name"])
-            .try(:authenticate, params["user"]["name"])
+  def create
+    
+    @user = User.find_by(name: session_params[:name])
+    if @user&.authenticate(session_params[:password])
+      login!
+      render json: { status: 'created', logged_in: true, data: @user }, status: 201
+    else
+      render json: { status: 401 }, status: 401
+    end
+  end
 
-    if user
-      session[:user_id] = user.id
-      render json: {
-        status: :created,
-        logged_in: true,
-        user: user
-      }
-    else 
-      render json: { status: 401 }
-    end 
-  end 
+  def logged_in?
+    if @current_user
+      render json: { logged_in: true, user: current_user }, status: 201
+    else
+      render json: { logged_in: false, message: 'no such user' }, status: 401
+    end
+  end
+
+  def destroy
+    logout!
+    render json: { status: 200, logged_out: true }
+  end
+
+  private
+
+  def session_params
+    params.permit(:name, :password)
+  end
 end
+
+
