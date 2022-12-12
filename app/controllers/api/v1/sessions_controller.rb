a7f365b8825a0dbef3d794d4073300089be9395f
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-12-12 20:01:25 +0100

    Update signup.js file

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,10 +2,9 @@ class Api::V1::SessionsController < ApplicationController
   def create 
     user = User
             .find_by(name: params["user"]["name"])
-            .try(:authenticate, params["user"]["name"])
+            .try(:authenticate, params["user"]["password"])
 
     if user
-      session[:user_id] = user.id
       render json: {
         status: :created,
         logged_in: true,
