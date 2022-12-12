fa0114882c163fc25deb8b7dd9c694a5c3edf25a
Tarek <83262890+tarek102@users.noreply.github.com>
2022-12-12 14:06:01 +0200

    Modify regestration controller

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,22 +1,18 @@
 class Api::V1::RegistrationsController < ApplicationController
   def create
-    user = User.create!(
-      name: params['user']['name'],
-      email: params['user']['email'],
-      password: params['user']['password'],
-      password_confirmation: params['user']['password_confirmation']
-  )
-   
-    if user
-      session[:user_id] = user.id
-      render json: {
-        status: :created,
-        user: user
-      }
+    user = User.create(user_params)
+    if user.save
+      render json: { status: 'created', message: 'Saved Patient', data: user }
     else
-      render json: { 
-        status: 500 
-      }
+      render json: { status: 500, message: 'Patient not saved', errors: user.errors }
     end
   end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
 end
+
+
