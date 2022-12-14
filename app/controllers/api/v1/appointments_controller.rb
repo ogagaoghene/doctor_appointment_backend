3f12d3cbf1ef8f6b43a9f70795b6200df6926520
Tarek <83262890+tarek102@users.noreply.github.com>
2022-12-14 13:19:23 +0200

    show appointments

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,2 +1,40 @@
-class Api::V1::AppointmentsController < ApplicationController
-end
+
+    class Api::V1::AppointmentsController < ApplicationController
+      def index
+        appointments = Appointment.order('created_at ASC')
+        render json: { status: 'SUCCESS', message: 'Loaded Appointmet list', data: appointments }, status: :ok
+      end
+
+      def create
+        appointment = Appointment.new(appointment_params)
+        if appointment.save
+          render json: { status: 'SUCCESS', message: 'Saved Appointment', data: appointment }, status: 200
+        else
+          puts appointment.errors.inspect
+          render json: { status: 'ERROR', message: 'Appointment not saved', data: appointment.errors },
+                 status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        appointment = Appointment.find(params[:id])
+        appointment.destroy
+        render json: { status: 'SUCCESS', message: 'Deleted appointment', data: appointment }, status: :ok
+      end
+
+      def update
+        appointment = Appointment.find(params[:id])
+        if appointment.update_attributes(appointment_params)
+          render json: { status: 'SUCCESS', message: 'Updated Appointment', data: appointment }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Appointment not updated', data: appointment.errors },
+                 status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def appointment_params
+        params.require(:appointment).permit(:user_id, :doctor_id, :doctor_name, :appointment_time, :location)
+      end
+    end
--- config/routes.rb
+++ config/routes.rb
@@ -10,17 +10,21 @@ Rails.application.routes.draw do
 
   namespace :api do 
      namespace :v1 do
-      resources :appointments
+      resources :appointments, only: [:create, :index, :destroy]
      end 
   end 
 
   namespace :api do 
      namespace :v1 do
-      resources :doctors
+      resources :doctors, only: [:index, :show]
      end 
   end 
 
   resources :users
 
+  get "api/v1/doctors", to: "api/v1/doctors#index"
+  get "api/v1/doctors/:id", to: "api/v1/doctors#show"
+  post "/appointments", to: "api/v1/appointments#create"
+  get "api/v1/appointments/:user_id", to: "api/v1/appointments#index"
   post "/sign_in_form", to: "sessions#create"
 end
