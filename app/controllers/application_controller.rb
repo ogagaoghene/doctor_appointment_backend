ec07b5bb378b584cf1534731ed8510de2995f6d3
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-12-14 13:45:28 +0100

    Fix linting errors and setup rspec for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,2 @@
-class ApplicationController < ActionController::API
-  include ActionController::Helpers
-  skip_before_action :verify_authenticity_token, raise: false
-  helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!
-  
-  def login!
-    session[:name] = @user.name
-    puts session[:name]
-  end
-
-  def logged_in?
-    !!session[:name]
-  end
-
-  def current_user
-    @current_user ||= User.find(session[:name]) if session[:name]
-  end
-
-  def authorized_user?
-    @user == current_user
-  end
-
-  def logout!
-    session.clear
-  end
-
+class ApplicationController < ActionController::Base
 end
