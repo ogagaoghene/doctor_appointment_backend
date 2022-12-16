0c92fcce6c9b398701b7e0ce39d0125e141f13bf
Tarek <83262890+tarek102@users.noreply.github.com>
2022-12-16 16:51:23 +0200

    Fix Rubocop errors

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -4,7 +4,6 @@ class Doctor < ApplicationRecord
   has_many :appointments, dependent: :destroy
   has_many :users, through: :appointments, dependent: :destroy
 
-
   has_one_attached :image
   validates :name, presence: true, length: { in: 3..50, message: 'Name\'s length should be in range of 3 to 50' }
   validates :specialty, presence: true,
