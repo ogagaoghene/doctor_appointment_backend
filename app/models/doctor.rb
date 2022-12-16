9541dc5708b48b74a8371b051c3303d8e26bd38a
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-12-16 14:32:23 +0100

    Fix linting issues

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,7 +2,7 @@
 
 class Doctor < ApplicationRecord
   has_many :appointments, dependent: :destroy
-  
+
   has_one_attached :image
   validates :name, presence: true, length: { in: 3..50, message: 'Name\'s length should be in range of 3 to 50' }
   validates :speciality, presence: true,
